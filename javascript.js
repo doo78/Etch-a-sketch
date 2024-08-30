fbbf439a920c89b402dc8d9f3a9e7aacc2becd89
doo78 <rickydeangordon@gmail.com>
2024-08-30 20:34:57 +0100

    Added the functionality to draw and change the amount of squares in the grid

--- javascript.js
+++ javascript.js
@@ -1,21 +1,31 @@
 
-const box = document.createElement("div");
 const container = document.querySelector(".container");
 
 
 
-box.classList.add("box");
 
-function createGrid(){
+
+function createGrid(size){
 
     function createRow(){
+
+        const box = document.createElement("div");
+        box.classList.add("box");
+       
+        
 
         const boxRow = document.createElement("div");
         boxRow.setAttribute("id", "boxRow");
 
-        for (let i = 0; i < 16; i++){
+        for (let i = 0; i < size; i++){
             const boxToAdd = document.createElement("div");
             boxToAdd.classList.add("box")
+
+            let heightWidth = Math.floor((400/size));
+
+            boxToAdd.style.height = heightWidth + "px";
+            boxToAdd.style.width = heightWidth + "px";
+
             boxRow.appendChild(boxToAdd);
         }
 
@@ -23,10 +33,43 @@ function createGrid(){
     }
 
 
-    for (let i = 0; i < 16; i++){
+    for (let i = 0; i < size; i++){
         
         container.appendChild(createRow());
     }
+    initalizeBox();
 }
 
-createGrid();
+
+
+
+createGrid(16);
+
+function initalizeBox(){
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "blue";
+        });
+    });
+}
+
+
+const sizeButton = document.querySelector("#size");
+
+sizeButton.addEventListener("click", () => {
+    let size = prompt("Enter the number of squares per side you want: ");
+
+    if (size > 100){
+        alert("The max size is 100.");
+    }
+
+    else{
+        while (container.hasChildNodes()){
+            container.removeChild(container.lastChild);
+        }
+
+        
+        createGrid(size);
+    }
+});
